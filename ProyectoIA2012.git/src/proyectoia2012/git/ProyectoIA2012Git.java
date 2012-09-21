37f56da7e30f8cef64a6a200f3179dad2f820cca
omarvides <omarvides@gmail.com>
2012-09-20 20:27:35 -0700

    Creacion de proyecto e Interfaz grafica Basica.

---
+++ ProyectoIA2012.git/src/proyectoia2012/git/ProyectoIA2012Git.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoia2012.git;
+
+/**
+ *
+ * @author Omar
+ */
+public class ProyectoIA2012Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
